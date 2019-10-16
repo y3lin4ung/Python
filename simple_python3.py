a36791a5ef491ed0e33fdbae47e62fb7c3498d23
Ye Lin Aung <yelin.aung5991@gmail.com>
2019-10-16 13:52:57 +0630

    Python3 Simple Template

---
+++ simple_python3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from sys import argv
+import base64
+
+def function_one():
+    try:
+        script, mode, cipher = argv
+        if mode == "-u":
+            print("Result")
+            print(res)
+        elif mode == "-b":
+            b64_string += "=" * ((4 - len(b64_string) % 4) % 4)
+    except:
+        print("Usage: python3 decoding -u | -b xxxxx")
+        print()
+    for i in range(len(detected)):
+        print(i)
+
+def mian():
+    global gusername,gpwd
+    function_one()
+   
+
+if __name__ == "__main__":
+    main()
+
+
+
